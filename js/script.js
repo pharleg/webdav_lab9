3b5fbeab0d0ba13d6d40445cbe7185d288e6b700
pharleg <curtis.mcewen@gmail.com>
2024-03-04 13:35:02 -0500

    Update script again
    still had the name wrong (textentry)

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
   var button = document.getElementById('entrybutton');
 
   function showMeText(){
-    var textbox = document.getElementById('textinput');
+    var textbox = document.getElementById('textentry');
     var alertMessage = "Curtis McEwen: " + textbox.value;
     alert(alertMessage);
     document.getElementById('textoutput').innerHTML = textbox.value;
