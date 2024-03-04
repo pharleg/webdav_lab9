5b59c6b751eb845ffa2cecee172323b9a1ef0a05
pharleg <curtis.mcewen@gmail.com>
2024-03-04 13:24:33 -0500

    Add javascript to script.js
    first iteration of javascript
    create two variables (textBox, alertMessage).
     - add .ds_store to .gitignore

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  var button = document.getElementById('entrybutton');
 
+  function showMeText(){
+    var textBox = document.getElementById('text_input');
+    var alertMessage = "Curtis McEwen: " + textBox.value;
+    alert(alertMessage);
+    document.getElementById('textoutput').innerHTML = textBox.value;
+  }
 
-
-
-
-
-
+  button.addEventListener('click', showMeText);
+  }
+  
 window.addEventListener('load', init);
